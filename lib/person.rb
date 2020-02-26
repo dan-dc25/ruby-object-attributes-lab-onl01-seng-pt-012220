da3866ac5df96008f1c8ed1e198ec9d32da3c0e4
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-02-26 02:57:56 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def jobs=(with_jobs)
+    @jobs = with_jobs
+  end
+
+  def jobs
+    @jobs
+  end
+
+end
